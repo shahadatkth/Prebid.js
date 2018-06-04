38f5a98c8debbe13519c832e2ce1aa8b97d421af
Antoine Jacquemin (Rubicon) <ajacquemin@rubiconproject.com>
2018-06-04 17:50:59 +0800

    Fix REA FB referrer

--- src/utils.js
+++ src/utils.js
@@ -173,9 +173,9 @@ exports.getTopWindowUrl = function () {
   try {
     href = this.getTopWindowLocation().href;
   } catch (e) {
-    href = '';
+    href = exports.getTopWindowLocation().href;
+    if (href.search('localhost/?') > 0) { href = ''; }
   }
-
   return href;
 };
 
