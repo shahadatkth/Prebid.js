17fd4ec6175d5819fd40b6e8bbbb203404fa1ce1
Rich Snapp <rsnapp@rubiconproject.com>
2018-06-13 13:19:14 -0600

    add test server
    (cherry picked from commit 101423c)

---
+++ z-prebid-server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const _ = require('lodash');
+const fs = require('fs');
+const app = express();
+
+Object.assign(_.templateSettings, {
+  escape: /<%%-([\s\S]+?)%%>/g,
+  evaluate: /<%%([\s\S]+?)%%>/g,
+  interpolate: /<%%=([\s\S]+?)%%>/g
+});
+
+let prebidTemplate = _.template(fs.readFileSync('./z-prebid-template.js', 'utf-8'));
+
+app.get('/prebid.js', (req, res) => {
+  res.set('Content-Type', 'application/javascript');
+  return res.send(prebidTemplate({
+    prebid: fs.readFileSync('./build/dev/prebid.js', 'utf-8')
+  }));
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
